3ae6fc9f063a4701b741d2b0aa98b752628dcb14
czjone@126.com <czjone@126.com>
2019-01-25 10:52:28 +0100

    add thread supprots.

--- bcpboost.sh
+++ bcpboost.sh
@@ -4,6 +4,6 @@ cd boost_1_66_0
 sh bootstrap.sh
 ./bjam tools/bcp
 mkdir -p bcpout
-dist/bin/bcp asio.hpp serialization/serialization.hpp bind/bind.hpp bcpout
+dist/bin/bcp asio.hpp serialization/serialization.hpp thread.hpp bind/bind.hpp bcpout
 cd ..
 echo bcp complate !
