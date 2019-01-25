e7fce7de83c95bc5f3b509f4969c970378a0b822
root <root@localhost.localdomain>
2019-01-25 04:15:17 +0100

    add static library tools for linux

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 sh bcpboost.sh
-sh buildios.sh
-sh buildmac.sh
-sh buildandroid.sh
+# sh buildios.sh # build ios
+# sh buildmac.sh  #build mac
+# sh buildandroid.sh #build android
+sh buildlinux.sh #build linux
 mv boost_1_66_0/stage bin
 rm -rf bin/boost
 cp -rf boost_1_66_0/bcpout/boost bin/boost
