16c3ada0ebf490bb70cb27206cecdb88eae35689
czjone@126.com <czjone@126.com>
2019-01-29 03:35:48 +0100

    add boost log supports.

--- bcpboost.sh
+++ bcpboost.sh
@@ -3,7 +3,27 @@ echo bcp the boost lib.
 cd boost_1_66_0
 sh bootstrap.sh
 ./bjam tools/bcp
+
 mkdir -p bcpout
-dist/bin/bcp asio.hpp serialization/serialization.hpp thread.hpp bind/bind.hpp bcpout
+#bsc command begin
+dist/bin/bcp \
+asio.hpp \
+serialization/serialization.hpp \
+thread.hpp \
+bind/bind.hpp \
+ref.hpp \
+bind.hpp \
+log/core.hpp \
+log/common.hpp \
+log/attributes.hpp \
+log/core/record.hpp \
+log/utility/setup/common_attributes.hpp \
+log/expressions.hpp \
+log/sinks/text_ostream_backend.hpp \
+log/support/date_time.hpp \
+log/utility/setup/file.hpp \
+core/null_deleter.hpp \
+bcpout
+#bcp command end
 cd ..
 echo bcp complate !
